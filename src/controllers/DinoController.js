704aeaecbce6d835935e4cd75ed0281cffb83c6e
Frederick Roegiers <frederick@rogerthat.be>
2025-02-19 15:44:28 +0100

    wip

--- src/controllers/DinoController.js
+++ src/controllers/DinoController.js
@@ -10,7 +10,18 @@ export const index = (req, res) => {
 };
 
 export const detail = (req, res) => {
+  const dino = dinosaurs.find((dino) => dino.slug == req.params.slug);
+
+  if (!dino) {
+    res.status(404).render("errors/404", {
+      layout: "layouts/error",
+      message: "Dinosaur Not Found",
+    });
+  }
+
   res.render("dinosaurs/detail", {
+    title: "Dinosaur Details",
+    dino,
     navItems,
   });
 };
